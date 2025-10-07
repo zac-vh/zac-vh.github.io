f4a12fdac05616ad6eb10ece511f1be4e7027080
zac-vh <zchrvh@gmail.com>
2025-10-07 17:22:31 +0000

    Deploying to gh-pages from @ zac-vh/zac-vh.github.io@91e89411290c41bbf0947b473f3cc833411de3f3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-repositories",
+  },{id: "nav-projects",
+          title: "projects",
+          description: "A growing collection of your cool projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
+        },{id: "nav-repositories",
           title: "repositories",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
           section: "Navigation",
